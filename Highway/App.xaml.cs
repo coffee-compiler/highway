72cc92aa1b13114f2481ab6abb8657526c49f938
coffee-compiler <coffee-compiler@vrapped.de>
2024-11-07 18:47:37 +0100

    init maui blazor hybrid project

---
+++ Highway/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Highway
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Highway/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Highway
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
